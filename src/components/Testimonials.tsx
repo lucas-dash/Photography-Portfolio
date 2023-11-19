6acea115646d0c9ca2563e0f722979bd4e260f79
lucas-dash <homolka.lukas10@gmail.com>
2023-11-19 16:09:08 +0100

    counting numbers

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -1,24 +1,34 @@
+import useCounter from '@/Hooks/useCounter';
 import { Button } from './ui/button';
 
 export default function Testimonials() {
+  const clients = useCounter(20, 1.5);
+  const experience = useCounter(10, 1.5);
+  const awards = useCounter(3, 0.5);
+
   return (
     <section className="flex flex-col items-center gap-5 md:gap-10 md:px-16 md:w-max ">
       <article className="flex items-start gap-6 w-full justify-center md:justify-start">
-        <div className="flex flex-col items-center">
-          <p className="font-bold text-3xl md:text-4xl">10+</p>
-          <p className="text-text-gray text-xs">Experience</p>
+        <div className="flex flex-col items-center px-1">
+          <p className="font-bold text-3xl md:text-4xl">{experience}+</p>
+          <p className="text-text-gray text-xs text-center">
+            Years of <br /> Experience
+          </p>
         </div>
-        <div className="flex flex-col items-center">
-          <p className="font-bold text-3xl md:text-4xl">20</p>
+        <div className="flex flex-col items-center px-1">
+          <p className="font-bold text-3xl md:text-4xl">{clients}</p>
           <p className="text-text-gray text-xs">Clients</p>
         </div>
-        <div className="flex flex-col items-center">
-          <p className="font-bold text-3xl md:text-4xl">9.8</p>
-          <p className="text-text-gray text-xs">Project Rate</p>
+        <div className="flex flex-col items-center px-1">
+          <p className="font-bold text-3xl md:text-4xl">{awards}</p>
+          <p className="text-text-gray text-xs">Awards</p>
         </div>
       </article>
 
-      <Button variant={'ghost'} className="border border-secondary rounded-xl">
+      <Button
+        variant={'ghost'}
+        className="border border-secondary rounded-xl hover:bg-secondary hover:text-text-light"
+      >
         Discover
       </Button>
     </section>
